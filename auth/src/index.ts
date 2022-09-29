3518642f6764bb0af22a31c3817553bea1a74b02
Renato D. Schneider <renato@brasilfone.com.br>
2022-09-29 15:19:58 -0300

    feat: :fire: response normalization strategies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,28 @@
-import express from 'express'
+import express from "express";
+import { json } from "body-parser";
 
-const app = express()
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import 'express-async-errors'
 
-app.use(express.json())
+const app = express();
+app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!')
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('Server started on porasdasdt 3000asd!')
-})
+  console.log("Listening on port 3000!!!!!!!!");
+});
